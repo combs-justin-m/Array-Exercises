ab072643ca1fd1670f2b9509961a9003eb176fde
Justin Combs <combs.justin.m@gmail.com>
2015-05-17 21:31:47 -0400

    Update exercises.js

--- exercises.js
+++ exercises.js
@@ -27,7 +27,6 @@ function maxOfThree(num1, num2, num3){
 // ---------------------
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 // ---------------------
-var vowels = "aeiou"
 
 function isVowel(char){
     return "aeiou".indexOf(char) >= 0 ;
